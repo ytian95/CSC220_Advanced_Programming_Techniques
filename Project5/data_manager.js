5363c3c433c6f726f511c94c7a2dc2cebc2a9e2d
ytian95 <ytian95@gmail.com>
2015-11-06 17:19:13 -0500

    Data Manager parses data correctly

--- Project5/data_manager.js
+++ Project5/data_manager.js
@@ -1,12 +1,15 @@
 function DataManager(){
-    this.data = [];
+    this.data = {"columns":[]};
     this.initialize();
 }
 DataManager.prototype.initialize = function(){
-    this.httpRequest("sampleData.json");
+    this.httpRequest("sqftElecData.json", "kwh_total_sqft");
+    this.httpRequest("sqftGasData.json", "therms_total_sqft");
+    console.log(this.data);
 }
 
-DataManager.prototype.httpRequest = function(fileName){
+DataManager.prototype.httpRequest = function(fileName, dataType){
+    var thisFunc = this;
     var xmlhttp;
     if (window.XMLHttpRequest){
         xmlhttp = new XMLHttpRequest();
@@ -14,30 +17,43 @@ DataManager.prototype.httpRequest = function(fileName){
     else{
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    xmlhttp.open("GET", fileName, true);
+        
     xmlhttp.onreadystatechange = function(){
         if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
             var myArr = JSON.parse(xmlhttp.responseText);
-            myFunction(myArr);
+            //console.log(myArr);
+            thisFunc.parseData(myArr, dataType);
         }
-    }
-    xmlhttp.open("GET", fileName, true);
+    };
     xmlhttp.send();
 }
 
-function myFunction(arr) {
-    var out = "";
-    var i;
-    var end = 10;
-    if(arr.length < end){
-        end = arr.length;
+DataManager.prototype.parseData = function(myArr, dataType){
+    var columns = myArr.meta.view.columns;
+    var communityIndex = 0;
+    var dataIndex = 0;
+    for(var i = 0; i < columns.length; i++){
+        if(columns[i].fieldName === "community_area_name"){
+            communityIndex = i;
+        }
+        if(columns[i].fieldName === dataType){
+            dataIndex = i;
+        }
     }
-    for(i = 0; i < end; i++) {
-        console.log(arr[i]);
-        console.log(arr[i].phone.home);
-//        out += '<a href="' + arr[i].url + '">' + 
-//        arr[i].display + '</a><br>';
+    this.data.columns.push(dataType);
+    this.data[dataType] = [];
+    for(var i = 0; i < myArr.data.length; i++) {
+        var communityName = myArr.data[i][communityIndex];
+        var datum = myArr.data[i][dataIndex];
+        var dataPoint = new Datum(communityName, datum);
+        this.data[dataType].push(dataPoint);
     }
-//    document.getElementById("id01").innerHTML = out;
+}
+
+function Datum(name, dataPiece){
+    this.name = name;
+    this.dataPiece = dataPiece;
 }
 
 function initialize(){
